b32bcecfa1f212043638bfbb016a9333e50a3109
Kairos <153609237+Background-Character-A@users.noreply.github.com>
2026-03-31 13:33:01 +0800

    Add files via upload

---
+++ student_db/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const express = require('express');
+const students = require("./db/students");
+
+const app = express();
+app.use(express.urlencoded({'extended':false}));
+app.use(express.json());
+
+
+app.use("/students",students)
+
+
+app.get("/",(req,res)=>{
+	return res.json({'message':'hello world'});
+});
+
+const server = app.listen(port,()=>{
+	require('dns').lookup(require('os').hostname(),(err,addr,fam)=>{
+		console.log(`listening at http://${addr}:${port}`);
+	});
+});
